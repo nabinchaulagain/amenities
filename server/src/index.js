b8fd6a435b388f09fd684c1b4a6cbc164469aa75
nabinchaulagain <wrighteric002@gmail.com>
2021-03-01 17:59:26 +0545

    fix cors issue

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const authRoutes = require('./routes/auth.route');
 const handleError = require('./middlewares/handleError');
 const parseUser = require('./middlewares/parseUser');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(parseUser);
