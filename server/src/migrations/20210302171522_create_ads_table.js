c932d84fc6e6d708871b776564acd9d4208bc3ec
nabinchaulagain <wrighteric002@gmail.com>
2021-03-02 17:35:05 +0545

    add migration for ads table

---
+++ server/src/migrations/20210302171522_create_ads_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('ads', (table) => {
+    table.increments('id');
+    table.string('title', 32).notNullable();
+    table.text('description').notNullable();
+    table.integer('price').notNullable();
+    table.string('location', 128).notNullable();
+    table.string('image', 128).notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.integer('user_id').notNullable();
+    table.foreign('user_id').references('users.id');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('ads');
+};
