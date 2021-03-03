e59a5e2a76ade5558bcb36363a61eca57a66d2d6
nabinchaulagain <wrighteric002@gmail.com>
2021-03-03 17:36:27 +0545

    add ad actions and reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from './reducers/auth.reducer';
+import adReducer from './reducers/ad.reducer';
 
-const reducers = combineReducers({ auth: authReducer });
+const reducers = combineReducers({ auth: authReducer, ad: adReducer });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
